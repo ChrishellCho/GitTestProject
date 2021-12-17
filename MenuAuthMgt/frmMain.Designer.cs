614ede4e6afc1bd6e385d6c95d89c758972bdc58
YeJin Cho <aamy_98@naver.com>
2021-12-17 10:26:24 +0900

    초기 커밋.

---
+++ MenuAuthMgt/frmMain.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace MenuAuthMgt
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.treeView1 = new System.Windows.Forms.TreeView();
+			this.flowLayoutPanel1.SuspendLayout();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+			this.menuStrip1.Size = new System.Drawing.Size(914, 30);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.Controls.Add(this.button1);
+			this.flowLayoutPanel1.Controls.Add(this.button2);
+			this.flowLayoutPanel1.Controls.Add(this.button3);
+			this.flowLayoutPanel1.Controls.Add(this.panel1);
+			this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 30);
+			this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(229, 711);
+			this.flowLayoutPanel1.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.button1.Location = new System.Drawing.Point(0, 0);
+			this.button1.Margin = new System.Windows.Forms.Padding(0);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(224, 45);
+			this.button1.TabIndex = 0;
+			this.button1.Tag = "0";
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+			this.button2.Location = new System.Drawing.Point(0, 45);
+			this.button2.Margin = new System.Windows.Forms.Padding(0);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(224, 45);
+			this.button2.TabIndex = 1;
+			this.button2.Tag = "1";
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button3
+			// 
+			this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+			this.button3.Location = new System.Drawing.Point(0, 90);
+			this.button3.Margin = new System.Windows.Forms.Padding(0);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(224, 45);
+			this.button3.TabIndex = 2;
+			this.button3.Tag = "2";
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.treeView1);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panel1.Location = new System.Drawing.Point(3, 139);
+			this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(221, 625);
+			this.panel1.TabIndex = 3;
+			// 
+			// treeView1
+			// 
+			this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.treeView1.Location = new System.Drawing.Point(0, 0);
+			this.treeView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.treeView1.Name = "treeView1";
+			this.treeView1.Size = new System.Drawing.Size(221, 625);
+			this.treeView1.TabIndex = 0;
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(914, 741);
+			this.Controls.Add(this.flowLayoutPanel1);
+			this.Controls.Add(this.menuStrip1);
+			this.IsMdiContainer = true;
+			this.MainMenuStrip = this.menuStrip1;
+			this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.Name = "frmMain";
+			this.Text = "frnMain";
+			this.Load += new System.EventHandler(this.frmMain_Load);
+			this.flowLayoutPanel1.ResumeLayout(false);
+			this.panel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TreeView treeView1;
+	}
+}
---
+++ MenuAuthMgt/frmMain.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MenuAuthMgt
+{
+	public partial class frmMain : Form
+	{
+		string userID;
+
+		public frmMain(string userID)
+		{
+			InitializeComponent();
+			this.userID = userID;
+		}
+
+		private void frmMain_Load(object sender, EventArgs e)
+		{
+			//아이디로 접근할 수 있는 메뉴 동적 바인딩
+			MenuDAC db = new MenuDAC();
+			DataTable dtMenu = db.GetUserMenuList(this.userID);
+
+			DrawMenuStrip(dtMenu);
+			DrawMenuPanel(dtMenu);
+		}
+
+		private void DrawMenuPanel(DataTable dtMenu)
+		{
+			DataView dv1 = new DataView(dtMenu);
+			dv1.RowFilter = "menu_level=1";
+			dv1.Sort = "menu_sort";
+			for (int i = 0; i < dv1.Count; i++)
+			{
+				ToolStripMenuItem p_menu = new ToolStripMenuItem();
+				p_menu.Name = $"p_menu{dv1[i]["menu_id"].ToString()}";
+				p_menu.Text = dv1[i]["menu_name"].ToString();
+				p_menu.Size = new Size(180, 22);
+
+				DataView dv2 = new DataView(dtMenu);
+				dv2.RowFilter = "menu_level = 2 and pnt_menu_id=" + dv1[i]["pnt_menu_id"].ToString();
+				dv2.Sort = "menu_sort";
+
+				for (int k = 0; k < dv1.Count; k++)
+				{
+					Button
+
+					this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+					this.button1.Location = new System.Drawing.Point(0, 0);
+					this.button1.Margin = new System.Windows.Forms.Padding(0);
+					this.button1.Name = "button1";
+					this.button1.Size = new System.Drawing.Size(196, 36);
+					this.button1.TabIndex = 0;
+					this.button1.Tag = "0";
+					this.button1.Text = "button1";
+					this.button1.UseVisualStyleBackColor = true;
+					this.button1.Click += new System.EventHandler(this.button1_Click);
+				}
+
+			}
+
+			private void DrawMenuStrip(DataTable dtMenu)
+			{
+				DataView dv1 = new DataView(dtMenu);
+				dv1.RowFilter = "menu_level=1";
+				dv1.Sort = "menu_sort";
+				for (int i = 0; i < dv1.Count; i++)
+				{
+					ToolStripMenuItem p_menu = new ToolStripMenuItem();
+					p_menu.Name = $"p_menu{dv1[i]["menu_id"].ToString()}";
+					p_menu.Text = dv1[i]["menu_name"].ToString();
+					p_menu.Size = new Size(180, 22);
+
+					DataView dv2 = new DataView(dtMenu);
+					dv2.RowFilter = "menu_level = 2 and pnt_menu_id=" + dv1[i]["pnt_menu_id"].ToString();
+					dv2.Sort = "menu_sort";
+
+					for (int k = 0; k < dv1.Count; k++)
+					{
+						ToolStripMenuItem c_menu = new ToolStripMenuItem();
+						p_menu.Name = $"p_menu{dv1[i]["menu_id"].ToString()}";
+						p_menu.Text = dv1[i]["menu_name"].ToString();
+						p_menu.Size = new Size(180, 22);
+
+						p_menu.DropDownItems.Add(c_menu);
+					}
+
+					this.menuStrip1.Items.Add(p_menu);
+				}
+			}
+
+			private void button1_Click(object sender, EventArgs e)
+			{
+				Button btn = (Button)sender;
+				flowLayoutPanel1.Controls.SetChildIndex(panel1, Convert.ToInt32(btn.Tag) + 1);
+				flowLayoutPanel1.Invalidate();
+			}
+
+		}
+	}
+}
